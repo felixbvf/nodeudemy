9d694f87a213e2e08c04d4f61c69e8063fc06759
felixbvf <felixbvf@gmail.com>
2019-09-20 18:47:40 -0400

    Create Controller and Routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'; //middleware morgan permite ver las peticiones desd
 import cors from 'cors'; //middleware cors permite conexiones a nuestro servidor desde otras clientes
 import path from 'path'; //te da l direccion del proyecto
 import mongoose from 'mongoose';
+import router from './routes'; //importando rutas
 
 //conexion a mongodb
 mongoose.Promise = global.Promise;
@@ -20,15 +21,18 @@ app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api',router);
 
-app.set('port', process.env.PORT || 3003);
+app.set('port', process.env.PORT || 3004);
 
-
+/*
 app.listen(app.get('port'), () => {
     console.log('Server on port ' + app.get('port'));
     console.log(path.join(__dirname, 'public')); //probando path
-});
-/*app.listen(3003, () => {
+});*/
+
+
+app.listen(3003, () => {
     console.log('Server on port 3003');
     console.log(path.join(__dirname, 'public')); //probando path
-});*/
+});
--- models/categoria.js
+++ models/categoria.js
@@ -2,9 +2,9 @@ import mongoose, {Schema} from 'mongoose';
 
 const categoriaSchema = new Schema({
     nombre: { type: String, maxlength: 50, unique:true, required:true},
-    descripcion: { type:String, maxlength:255 },
-    estado: {type: Number, default:1},
-    createAt: {type: Date, default:Date.now}
+    descripcion: { type: String, maxlength:255 },
+    estado: { type: Number, default:1},
+    createdAt: {type: Date, default:Date.now}
 });
 
 const Categoria = mongoose.model('categoria',categoriaSchema);
