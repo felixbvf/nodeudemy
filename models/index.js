94b43b2f275f9ce88f763ee3346213f4026a9b8c
felixbvf <felixbvf@gmail.com>
2019-09-25 18:20:39 -0400

    Created model venta

--- models/index.js
+++ models/index.js
@@ -3,10 +3,12 @@ import Articulo from './articulo';
 import Usuario from './usuario';
 import Persona from './persona';
 import Ingreso from './ingreso';
+import Venta from './venta';
 export default {
     Categoria,
     Articulo,
     Usuario,
     Persona,
-    Ingreso
+    Ingreso,
+    Venta
 }
