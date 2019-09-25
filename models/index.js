be5e4b4423b8df4435ee787738160d3802ab9a27
felixbvf <felixbvf@gmail.com>
2019-09-24 20:17:32 -0400

    created model persona

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 import Categoria from './categoria';
 import Articulo from './articulo';
 import Usuario from './usuario';
+import Persona from './persona';
 export default {
     Categoria,
     Articulo,
-    Usuario
+    Usuario,
+    Persona
 }
