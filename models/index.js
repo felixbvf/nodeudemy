6ae0b8d6f2651d7f1700b6c35b1b39d1ff627d37
felixbvf <felixbvf@gmail.com>
2019-09-25 15:18:17 -0400

    Created model ingreso

--- models/index.js
+++ models/index.js
@@ -2,9 +2,11 @@ import Categoria from './categoria';
 import Articulo from './articulo';
 import Usuario from './usuario';
 import Persona from './persona';
+import Ingreso from './ingreso';
 export default {
     Categoria,
     Articulo,
     Usuario,
-    Persona
+    Persona,
+    Ingreso
 }
